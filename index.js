17b02b66ddd88e202dce95a50d1499e6e43ceed9
devsubrata <devsubrata@gmail.com>
2023-09-10 02:13:58 +0600

    03 - Authentication with Google OAuth

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require('express');
-const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
-app.get('/', (req, res) => {
-    res.send({bye: 'there'});
-});
+const app = express();
+app.use(express.json());
+
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, 
+    (accessToken, refreshToken, profile, done) => {
+        console.log('Access Token: ', accessToken);
+        console.log('Refresh Token: ', refreshToken);
+        console.log('Profile: ', profile);
+
+        done();
+    })
+);
+
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }),
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
